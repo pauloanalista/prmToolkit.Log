f744e96ee3fa35b8cd6933f889449e48a79ad7fb
Paulo Rogério Martins Marques <paulo.analista@outlook.com>
2017-05-02 19:52:16 -0300

    Versionando prmToolkit.Log

---
+++ prmToolkit.Log/Interfaces/ILog.cs
@@ -0,0 +1,12 @@
+﻿using prmToolkit.Log.Enum;
+using System.Threading.Tasks;
+
+namespace prmToolkit.Log.Interfaces
+{
+    public interface ILog 
+    {
+        void Save(string message, EnumMessageType enumMessageType);
+
+        Task SaveAsync(string message, EnumMessageType enumMessageType);
+    }
+}
